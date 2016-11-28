5d7e4134c1c87cdcc31137bcc3bd8d8bd258691f
nexus161 <nexus.hedspi@gmail.com>
2016-11-28 08:00:52 +0000

    fig bug

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,8 +2,12 @@ class StaticPagesController < ApplicationController
 	before_action :authenticate_user!, only: [:profile,:manager_reviews]
 	before_action :require_admin,only: [:manager_reviews]
   def home
-      @new_movies = Movie.all
-      @most_likes = Review.order(:cached_votes_up => :desc).limit(2)
+        @new_movies = Movie.all
+        @most_rates = RatingCache.order(:avg => :desc).limit(4)
+        @max_movies = []
+        @most_rates.each do |most|
+            @max_movies << Movie.find_by_id(most.cacheable_id)
+        end
   end
   def profile
   	@user = current_user
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,6 +5,7 @@ class Movie < ActiveRecord::Base
   	has_attached_file :image, styles: { :medium => "640x" }
   	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   	has_many :reviews
+  	has_one  :avgs, class_name: "RatingCache",foreign_key: "cacheable_id"
   	belongs_to :category
   	def self.search(search)
   		where("name LIKE ?", "%#{search}%")
