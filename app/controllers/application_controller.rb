08c3ff541fd71361c7982445fbb5c319192a4562
nexus161 <nexus.hedspi@gmail.com>
2016-12-01 15:10:22 +0000

    fix bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
     @categories = Category.all
   end
   def after_sign_in_path_for(resource)
-    stored_location_for(resource) || movies_path
+    stored_location_for(resource) || root_path
   end
   protected
   def require_admin
